4faf7ab2b1b4b8629840dae9fb5762d62b6375ae
TekenCPS <131601746+TekenCPS@users.noreply.github.com>
2023-07-30 20:36:34 +0700

    Update events.cpp

--- proxy/events.cpp
+++ proxy/events.cpp
@@ -487,7 +487,7 @@ bool events::in::variantlist(gameupdatepacket_t* packet) {
         case fnv32("OnSendToServer"): g_server->redirect_server(varlist); return true;
 
         case fnv32("OnConsoleMessage"): {
-            varlist[1] = "`4[PROXY]`` " + varlist[1].get_string();
+            varlist[1] = "`0[`^ErzaProxy`0]`` " + varlist[1].get_string();
             auto cnsl = varlist[1].get_string();
           g_server->send(true, varlist);
        return true;
